8f4c75aed071e04d3bc11e731c40321d6769fa53
Taren P <63258880+TarenP@users.noreply.github.com>
2021-10-09 11:41:38 -0700

    Pieces can now be selected

--- ChessMain.py
+++ ChessMain.py
@@ -3,6 +3,9 @@ This is our main driver file. It will be responsible for handling user input and
 """
 
 import pygame as p
+import sys
+from pygame.event import set_blocked
+from pygame.locals import *
 import ChessEngine
 
 
@@ -31,40 +34,65 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    selected = [-1, -1]  #assign selected a coordinate value so I don't get any variable unassigned errors
     loadImages() #only do this once, before the while loop
     running = True
     while running:
         for e in p.event.get():
+            if e.type == MOUSEBUTTONDOWN:
+                if e.button == 1:
+                    print(mx)
+                    print(my)
+                    selected = selectPiece(mx, my, gs.board, screen)
+
             if e.type == p.QUIT:
                 running = False
-        drawGameState(screen, gs)
+
+        drawGameState(screen, gs, selected)
         
-                
+        mx, my = p.mouse.get_pos() #gets mouse position, only captures position when cursor is in pygame window
 
         clock.tick(MAX_FPS)
         p.display.flip()
 
-
-def userInput():
-    
-
+'''
+On click, go through all the pieces on the board and see which is touching the cursor so it can be selected. returns selected piece coordinates
+'''
+def selectPiece(mx, my, board, screen):
+    for r in range(DIMESION):
+        for c in range(DIMESION):
+            piece = board[r][c] #Get name of piece
+            #print(piece)
+            if piece != "--" and p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE).collidepoint(mx, my): #see if the cursor coords are within the piece's square
+                p.draw.rect(screen, p.Color("orange"), p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                selected = [r, c]
+                #print(selected)
+                return selected
+            elif piece == "--":
+                selected = [-1, 1]
+                #print(selected)
+    return selected
+            
 '''
 Responsible for all tje graphics within a current game state.
 '''
-def drawGameState(screen, gs):
-    drawBoard(screen) #draw squares on the board
+def drawGameState(screen, gs, selected):
+    drawBoard(screen, selected) #draw squares on the board
     #add in piece highlighting or move suggestions (later)
     drawPieces(screen, gs.board) #draw pieces on top of those sqaures
 
 '''
 Draw the squares on the board. The top left square is always light.
 '''
-def drawBoard(screen):
+def drawBoard(screen, selected):
     colors = [p.Color("white"), p.Color("dark green")]
     for r in range(DIMESION):
         for c in range(DIMESION):
-            color = colors[((r+c)%2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            if r == selected[0] and c == selected[1]:
+                p.draw.rect(screen, p.Color("orange"), p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            else:
+                color = colors[((r+c)%2)]
+                p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 '''
 Draw the pieces on the board using the current GameState.board
