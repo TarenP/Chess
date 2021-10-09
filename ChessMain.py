93cff128ccfe3095e00cc13eabbf0e39528f907d
Taren P <63258880+TarenP@users.noreply.github.com>
2021-10-08 18:50:57 -0700

    Working on initializing pictures of pieces

--- ChessMain.py
+++ ChessMain.py
@@ -1,3 +1,22 @@
 """
 This is our main driver file. It will be responsible for handling user input and displaying current GameState object.
 """
+
+import pygame as p
+from Chess-AI import ChessEngine
+
+
+WIDTH = HEIGHT = 512
+DIMESION = 8 #dimensions of the chess board are 8x8
+SQ_SIZE = HEIGHT // DIMESION
+MAX_FPS = 15 #for animations later on
+IMAGES = {}
+
+'''
+Initialize a global dictionary of images. This will be called exactly once in the main
+'''
+def loadImages():
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.image.load("ChessImages/" + piece + ".png")
+    #Note: We can access an image by saying 'IMAGES['wP']'
