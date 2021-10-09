387f1c582a80004b0ec90c6ca3a5bfa1f6b3765d
Taren P <63258880+TarenP@users.noreply.github.com>
2021-10-08 21:15:50 -0700

    Created basic pygame params

--- ChessMain.py
+++ ChessMain.py
@@ -3,7 +3,7 @@ This is our main driver file. It will be responsible for handling user input and
 """
 
 import pygame as p
-from Chess-AI import ChessEngine
+import ChessEngine
 
 
 WIDTH = HEIGHT = 512
@@ -18,5 +18,19 @@ Initialize a global dictionary of images. This will be called exactly once in th
 def loadImages():
     pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
-        IMAGES[piece] = p.image.load("ChessImages/" + piece + ".png")
+        IMAGES[piece] = p.transform.scale(p.image.load("ChessImages/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     #Note: We can access an image by saying 'IMAGES['wP']'
+
+'''
+The main driver for our code. This will handle user input and updating the graphics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    print(gs.board)
+
+main()
